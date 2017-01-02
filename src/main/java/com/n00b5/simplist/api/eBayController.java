7f7de5f2fe7d4eb43c3f482bb5da72353558e17c
Martino Nikolovski <nikolovski23@gmail.com>
2017-01-01 23:40:20 -0500

    Getting access token for eBay user.

--- src/main/java/com/n00b5/simplist/api/eBayController.java
+++ src/main/java/com/n00b5/simplist/api/eBayController.java
@@ -1,18 +1,17 @@
 package com.n00b5.simplist.api;
 
+import org.apache.commons.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import sun.misc.BASE64Encoder;
 
 import javax.net.ssl.HttpsURLConnection;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
+import java.io.*;
 import java.net.URL;
 import java.net.URLEncoder;
 
@@ -21,34 +20,59 @@ import java.net.URLEncoder;
  */
 @Controller
 @PropertySource("classpath:dev_ebay.properties")
-public class eBayController{
+public class eBayController {
     @Value("${appID}")
     private String clientID;
-    @Value("${redirectURL}")
-    private String redirectURL;
+    @Value("${certID}")
+    private String clientSecret;
+    @Value("${RuName}")
+    private String redirectURI;
     @Value("${signInURL}")
     private String signInURL;
+    @Value("${Base64String}")
+    private String base64String;
     @Value("${scope}")
     private String scope;
     private String responseType = "code";
 
-    @RequestMapping(value = "/oAuth/eBay", method = RequestMethod.POST)
-    @ResponseBody
-    public void getToken() throws IOException {
-        String code = null;
-        String getCodeURL = signInURL+"client_id="+
-                            clientID+"&response_type="+
-                            responseType+"&redirect_url="+
-                            redirectURL+"&scope=";
-        scope = URLEncoder.encode(scope,"UTF-8");
-        URL obj = new URL(getCodeURL+scope);
-        HttpsURLConnection connection = (HttpsURLConnection) obj.openConnection();
-        connection.setRequestMethod("POST");
-        connection.setDoOutput(true);
-        DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
+    @RequestMapping(value = "/oAuth/eBay")
+    public ModelAndView getCode() throws IOException {
+        String getCodeURL = signInURL + "?client_id=" +
+                clientID + "&response_type=" +
+                responseType + "&redirect_uri=" +
+                redirectURI + "&scope=" +
+                URLEncoder.encode(scope, "UTF-8");
+        return new ModelAndView("redirect:" + getCodeURL);
+    }
+
+    @RequestMapping(value = "/authorize", params = {"state", "code"})
+    public void getToken(String state, String code) throws IOException {
+        URL obj = new URL("https://api.sandbox.ebay.com/identity/v1/oauth2/token");
+        String parameters = "grant_type=authorization_code&" +
+                "&code=" + URLEncoder.encode(code, "UTF-8") +
+                "&redirect_uri=" + redirectURI;
+        HttpsURLConnection conn = (HttpsURLConnection) obj.openConnection();
+        conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+        conn.setRequestProperty("Authorization", "Basic " + base64String);
+        conn.setRequestMethod("POST");
+        conn.setDoOutput(true);
+        conn.setDoInput(true);
+        DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
+        wr.writeBytes(parameters);
         wr.flush();
         wr.close();
+        BufferedReader in =
+                new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        System.out.println(response.toString());
+        in.close();
     }
+
     @Bean
     public static PropertySourcesPlaceholderConfigurer propertyConfig() {
         return new PropertySourcesPlaceholderConfigurer();
